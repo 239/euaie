20258235d30f1ec69113a1d1e9e60a791a117b71
239 <239@pm.me>
2026-04-15 15:00:07 +0200

    ignore 2h time shifts if tolerance > 0

--- _api/src/main/kotlin/L0.kt
+++ _api/src/main/kotlin/L0.kt
@@ -13,7 +13,7 @@ data class L0(val path: String, val size: Long, val time: Long) {
     fun ep(a: L0) = path == a.path
     fun es(a: L0) = size == a.size
     fun et(a: L0) = if (tolerance == 0L) time == a.time else
-        abs(time - a.time).let { it <= tolerance || it == 3600000L }
+        abs(time - a.time).let { it <= tolerance || it == 3600000L || it == 7200000L }
     fun link() = time == LINK
     fun toLine() = "$path$D$size$D$time"
     fun toWord() = if (real) if (file) if (link()) "link" else "file" else "folder" else " "
