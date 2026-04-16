9b4c01825f6e2d75422ad5b6a376594f323e8190
239 <239@pm.me>
2026-04-16 15:32:18 +0200

    set hour shifts to ignore

--- _api/src/main/kotlin/L0.kt
+++ _api/src/main/kotlin/L0.kt
@@ -1,5 +1,6 @@
 package euaie
 
+import kotlin.collections.map
 import kotlin.math.*
 
 typealias M0 = Map<String, L0>
@@ -13,7 +14,7 @@ data class L0(val path: String, val size: Long, val time: Long) {
     fun ep(a: L0) = path == a.path
     fun es(a: L0) = size == a.size
     fun et(a: L0) = if (tolerance == 0L) time == a.time else
-        abs(time - a.time).let { it <= tolerance || shifts.any { x -> x == it } }
+        abs(time - a.time).let { it <= tolerance || hours.any { x -> x == it } }
     fun link() = time == LINK
     fun toLine() = "$path$D$size$D$time"
     fun toWord() = if (real) if (file) if (link()) "link" else "file" else "folder" else " "
@@ -24,7 +25,13 @@ data class L0(val path: String, val size: Long, val time: Long) {
         const val LINK = 239L
         val fake = L0("", 0, -1)
         var tolerance = 0L
-        var shifts = setOf(1, 2).map { it * 3600000L } //TODO set via option?
+        var hours = setOf(1, 2).map { it * 3600000L } //TODO set via option?
+
+        //            set(s) {
+//                field = s
+//                shifts = s.map { it * 3600000L }
+//            }
+//        private var shifts = emptyList<Long>()
         fun fromLine(line: String): L0? {
             val l = line.split(D)
             val p = l.getOrNull(0)
