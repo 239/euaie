6bc15b832fb95d42f13c5058b40ec34b54b57677
239 <239@pm.me>
2026-04-21 14:07:40 +0200

    ...

--- _api/src/test/kotlin/Tests.kt
+++ _api/src/test/kotlin/Tests.kt
@@ -8,11 +8,11 @@ class TestScan {
 
     @Test
     fun `including and excluding`() {
-        val s0 = Scan(root, emptySet(), emptySet(), "...", Task()).scan()
-        val s1 = Scan(root, setOf("include"), emptySet(), "...", Task()).scan()
-        val s2 = Scan(root, setOf("include"), setOf("::.exc"), "...", Task()).scan()
+        val s0 = Scan(root, emptySet(), emptySet(), "", Task()).scan()
+        val s1 = Scan(root, setOf("include"), emptySet(), "", Task()).scan()
+        val s2 = Scan(root, setOf("include"), setOf("::.exc"), "", Task()).scan()
         Scan.optionInsensitive = true
-        val s3 = Scan(root, setOf("include"), setOf("::.exc"), "...", Task()).scan()
+        val s3 = Scan(root, setOf("include"), setOf("::.exc"), "", Task()).scan()
         assertThat(s0.size).isEqualTo(7)
         assertThat(s1.size).isEqualTo(6)
         assertThat(s2.size).isEqualTo(5)
