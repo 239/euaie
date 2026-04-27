b9fd2850b7e5a70806bcded2c0c9f25967a4dad3
239 <239@pm.me>
2026-04-27 14:08:09 +0200

    expand filtering test

--- _api/src/test/kotlin/Tests.kt
+++ _api/src/test/kotlin/Tests.kt
@@ -13,9 +13,14 @@ class TestScan {
         val s2 = Scan(root, setOf("include"), setOf("::.exc"), "", Task()).scan()
         Scan.optionInsensitive = true
         val s3 = Scan(root, setOf("include"), setOf("::.exc"), "", Task()).scan()
+        val s4 = Scan(root, setOf("include:tY"), setOf("::.exc"), "", Task()).scan()
+        val s5 = Scan(root, setOf("iNcLuDe:tY:c"), setOf("::.eXc"), "", Task()).scan()
         assertThat(s0.size).isEqualTo(7)
         assertThat(s1.size).isEqualTo(6)
         assertThat(s2.size).isEqualTo(5)
         assertThat(s3.size).isEqualTo(4)
+        assertThat(s4.size).isEqualTo(3)
+        assertThat(s5.size).isEqualTo(2)
+        assertThat(s5.keys).containsExactly(setOf("included/empty.inc", "included/EmPtY.iNc"))
     }
 }
