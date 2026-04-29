e50f99ee32da990a596c2535447663864b47f2ac
239 <239@pm.me>
2026-04-29 17:06:27 +0200

    rework test for path filters

--- _api/src/test/kotlin/Tests.kt
+++ _api/src/test/kotlin/Tests.kt
@@ -8,19 +8,12 @@ class TestScan {
 
     @Test
     fun `including and excluding`() {
-        val s0 = Scan(root, emptySet(), emptySet(), "", Task()).scan()
-        val s1 = Scan(root, setOf("include"), emptySet(), "", Task()).scan()
-        val s2 = Scan(root, setOf("include"), setOf("::.exc"), "", Task()).scan()
         Scan.optionInsensitive = true
-        val s3 = Scan(root, setOf("include"), setOf("::.exc"), "", Task()).scan()
-        val s4 = Scan(root, setOf("include:tY"), setOf("::.exc"), "", Task()).scan()
-        val s5 = Scan(root, setOf("iNcLuDe:tY:c"), setOf("::.eXc"), "", Task()).scan()
-        assertThat(s0.size).isEqualTo(7)
-        assertThat(s1.size).isEqualTo(6)
-        assertThat(s2.size).isEqualTo(5)
-        assertThat(s3.size).isEqualTo(4)
-        assertThat(s4.size).isEqualTo(3)
-        assertThat(s5.size).isEqualTo(2)
-        assertThat(s5.keys).containsExactly(setOf("included/empty.inc", "included/EmPtY.iNc"))
+        Scan(root, setOf("iNc::C"), setOf("::.eXc"), "", Task()).scan().also {
+            assertThat(it.keys).containsExactly(setOf("included/EmPtY.iNc"))
+        }
+        Scan(root, setOf("o/n/l/y/::this"), setOf(":not:"), "", Task()).scan().also {
+            assertThat(it.keys).containsExactly(setOf("O/n/L/y/this"))
+        }
     }
 }
