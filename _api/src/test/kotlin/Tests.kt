a27ce2b1d7cc4b91b26037fde34be4dba62e1783
239 <239@pm.me>
2026-05-13 11:33:35 +0200

    add new filter test

--- _api/src/test/kotlin/Tests.kt
+++ _api/src/test/kotlin/Tests.kt
@@ -9,11 +9,14 @@ class TestScan {
     @Test
     fun `including and excluding`() {
         Scan.optionInsensitive = true
-        Scan(root, setOf("iNc::C"), setOf("::.eXc"), "", Task()).scan().also {
+        Scan(root, setOf("iNcLuDe::C"), setOf("::.eXc"), "", Task()).scan().also {
             assertThat(it.keys).containsExactly(setOf("included/EmPtY.iNc"))
         }
         Scan(root, setOf("o/n/l/y/::this"), setOf(":not:"), "", Task()).scan().also {
             assertThat(it.keys).containsExactly(setOf("O/n/L/y/this"))
+        }
+        Scan(root, setOf("inc-0::.inc"), setOf(":not"), "", Task()).scan().also {
+            assertThat(it.keys).containsExactly(setOf("inc-0/okay.inc"))
         }
     }
 }
