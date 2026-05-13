0d5aa2d96e9e0e839bf90f3ebb7a9d74f368ef0b
239 <239@pm.me>
2026-05-13 12:15:08 +0200

    enable reproducible jar again

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,3 @@
-//import java.nio.file.*
-//import java.nio.file.attribute.*
-
 plugins {
     alias(libs.plugins.kjvm) apply false
     alias(libs.plugins.kapt) apply false
@@ -23,16 +20,11 @@ allprojects {
         resolutionStrategy.cacheChangingModulesFor(1, "hours")
     }
 
-    //reproducible //TODO enable again / Unix only?
-//    tasks.withType<AbstractArchiveTask>().configureEach {
-//        isPreserveFileTimestamps = false
-//        isReproducibleFileOrder = true
-//        eachFile {
-//            permissions {
-//                val exe = Files.getPosixFilePermissions(file.toPath()).contains(PosixFilePermission.OWNER_EXECUTE)
-//                unix(if (exe) "755" else "644")
-//            }
-//        }
-//        dirPermissions { unix("755") }
-//    }
+    //reproducible jar
+    tasks.withType<AbstractArchiveTask>().configureEach {
+        isPreserveFileTimestamps = false
+        isReproducibleFileOrder = true
+        eachFile { permissions { unix("644") } }
+        dirPermissions { unix("755") }
+    }
 }
